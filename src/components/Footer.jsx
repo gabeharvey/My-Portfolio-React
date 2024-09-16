3cf5e4f6a16ea4db21251f4142a7fa15a47c93bc
Gabe Harvey <harvey7@gmail.com>
2024-09-16 02:03:39 -0500

    Added social media icons in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,27 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Box, Text, Link, Container, Stack } from '@chakra-ui/react';
+import { FaLinkedin, FaGithub, FaFacebook, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
-
   return (
     <Box as="footer" py="2rem" px="2rem" fontFamily="'Share Tech Mono', cursive" id="footer" position="relative" mt="150px">
       <Container>
         <Stack spacing={4} textAlign="center">
-          <Text>&copy; {new Date().getFullYear()} Gabe's React Portfolio. All rights reserved.</Text>
           <Stack direction="row" spacing={6} justify="center">
-            <Link href="https://www.linkedin.com/in/gabe-harvey-37ab90308/">
-              LinkedIn
+            <Link href="https://www.linkedin.com/in/gabe-harvey-37ab90308/" isExternal aria-label="LinkedIn">
+              <FaLinkedin size={24} />
             </Link>
-            <Link href="https://github.com/gabeharvey">
-              GitHub
+            <Link href="https://github.com/gabeharvey" isExternal aria-label="GitHub">
+              <FaGithub size={24} />
+            </Link>
+            <Link href="https://www.facebook.com/" isExternal aria-label="Facebook">
+              <FaFacebook size={24} />
+            </Link>
+            <Link href="https://www.instagram.com/" isExternal aria-label="Instagram">
+              <FaInstagram size={24} />
             </Link>
           </Stack>
+          <Text>&copy; {new Date().getFullYear()} Gabe's React Portfolio. All rights reserved.</Text>
         </Stack>
       </Container>
     </Box>
